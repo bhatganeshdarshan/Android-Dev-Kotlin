05c3dcfeba2d8b2a28085def9091ff1c0b85e851
Ganeshdarshan <bhatganeshdarshan10@gmail.com>
2023-09-22 14:15:49 +0530

    Number Count App

--- App1/app/src/main/java/com/example/app1/MainActivity.kt
+++ App1/app/src/main/java/com/example/app1/MainActivity.kt
@@ -2,11 +2,26 @@ package com.example.app1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val Clicked =
+        var count = 0
+        val text = findViewById<TextView>(R.id.textView)
+        val clicked = findViewById<Button>(R.id.myButton)
+        clicked.setOnClickListener {
+            count = count+ 1
+            text.text = count.toString()
+        }
+        val res= findViewById<Button>(R.id.reset)
+        res.setOnClickListener{
+            count = 0
+            text.text = count.toString()
+        }
+        Toast.makeText(this,"Made by Ganeshdarshan",Toast.LENGTH_LONG).show()
+        }
     }
-}
